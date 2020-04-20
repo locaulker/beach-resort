d261ecfd2b806e97c1cef79dae07a08f04647740
Lawrence <Caulker>
2020-04-20 15:23:09 -0400

    addedd react-router-dom

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Home = () => {
+	return (
+		<div>
+			Hello from <strong>Homepage</strong>
+		</div>
+	)
+}
+export default Home
---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Rooms = () => {
+	return (
+		<div>
+			Hello from <strong>Rooms</strong> page
+		</div>
+	)
+}
+
+export default Rooms
